f2ae306d164835a27cd5c475758847ebd666fbbf
Abigayle Franklin <abifanklin@gmail.com>
2019-02-21 21:06:51 -0600

    REDO

--- frontend/src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const GET_POSTS = 'GET_POSTS';
-export const NEW_POST = 'NEW_POST';
--- frontend/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import postReducer from './postreduce'
-
-export default combineReducers({
-    posts: postReducer
-});
